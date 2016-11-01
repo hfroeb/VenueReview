ff6b6bd34ec27003a5b633102339df57367e3436
hfroeb <hfroeb@gmail.com>
2016-11-01 13:12:14 -0500

    second commit

---
+++ src/main/java/com/theironyard/entities/User.java
@@ -0,0 +1,65 @@
+package com.theironyard.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by halleyfroeb on 11/1/16.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false, unique = true)
+    String username;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
